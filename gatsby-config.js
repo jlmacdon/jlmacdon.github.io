8b5df8649f80dc8d45da64d14f063160949a1805
Jennifer MacDonald <jennifermacdonald@Jennifers-MacBook-Pro.local>
2022-05-22 16:17:14 -1000

    installed eslint airbnb style guide

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,18 @@
+// gatsby-config.js
+
+const path = require("path");
+// Get paths of Gatsby's required rules, which as of writing is located at:
+// https://github.com/gatsbyjs/gatsby/tree/fbfe3f63dec23d279a27b54b4057dd611dce74bb/packages/
+// gatsby/src/utils/eslint-rules
+const gatsbyRequiredRules = path.join(
+  process.cwd(),
+  "node_modules",
+  "gatsby",
+  "dist",
+  "utils",
+  "eslint-rules"
+);
+
 module.exports = {
   siteMetadata: {
     title: "medApprise",
@@ -71,6 +86,19 @@ module.exports = {
       resolve: "gatsby-plugin-netlify-cms",
       options: {
         modulePath: `${__dirname}/src/cms/cms.js`,
+      },
+    },
+    {
+      resolve: "gatsby-plugin-eslint",
+      options: {
+        // Gatsby required rules directory
+        rulePaths: [gatsbyRequiredRules],
+        // Default settings that may be ommitted or customized
+        stages: ["develop"],
+        extensions: ["js", "jsx", "ts", "tsx"],
+        exclude: ["node_modules", "bower_components", ".cache", "public"],
+        // Any additional eslint-webpack-plugin options below
+        // ...
       },
     },
     {
